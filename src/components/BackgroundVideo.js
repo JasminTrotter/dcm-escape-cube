371524f35aeebcfed9432b8511230fde4b82d66e
Mikey Pinaud <michael.pinaud@gmail.com>
2019-10-27 02:45:00 -0700

    add video to all stages

--- src/components/BackgroundVideo.js
+++ src/components/BackgroundVideo.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from "prop-types";
-// import win from "../video/win.mp4";
-// import failed from "../video/fail.mp4";
-// import firstStage from "../video/first-stage.mp4";
+import win from "../video/win.mp4";
+import failed from "../video/fail.mp4";
+import firstStage from "../video/first-stage.mp4";
 import home from "../video/home.mp4";
-// import introStory from "../video/intro-story.mp4";
-// import secondStage from "../video/second-stage.mp4";
-// import thirdStage from "../video/third-stage.mp4";
+import introStory from "../video/intro-story.mp4";
+import secondStage from "../video/second-stage.mp4";
+import thirdStage from "../video/third-stage.mp4";
 import '../styles/BackgroundVideo.css';
 
 function BackgroundVideo({
@@ -27,32 +27,32 @@ function BackgroundVideo({
       }
       {intro &&
         <video className="video" loop autoPlay muted>
-          <source src={home} type="video/mp4" />
+          <source src={introStory} type="video/mp4" />
         </video>
       }
       {(gameBoard && !questionMatchOne) &&
         <video className="video" loop autoPlay muted>
-          <source src={home} type="video/mp4" />
+          <source src={firstStage} type="video/mp4" />
         </video>
       }
       {(gameBoard && questionMatchOne && !questionMatchTwo) &&
         <video className="video" loop autoPlay muted>
-          <source src={home} type="video/mp4" />
+          <source src={secondStage} type="video/mp4" />
         </video>
       }
       {(gameBoard && questionMatchTwo) &&
         <video className="video" loop autoPlay muted>
-          <source src={home} type="video/mp4" />
+          <source src={thirdStage} type="video/mp4" />
         </video>
       }
       {success &&
         <video className="video" loop autoPlay muted>
-          <source src={home} type="video/mp4" />
+          <source src={win} type="video/mp4" />
         </video>
       }
       {fail &&
         <video className="video" loop autoPlay muted>
-          <source src={home} type="video/mp4" />
+          <source src={failed} type="video/mp4" />
         </video>
       }
     </>
